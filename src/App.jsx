6c9753033ecc415a0689a1b29f5d8bc9bfa205ac
Sant99Z <santinod008@gmail.com>
2024-10-11 22:55:30 -0300

    Se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1> Login </h1>
+        
+        </div>
+    </>
+  )
+}
+
+export default App
