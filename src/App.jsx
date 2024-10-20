7e3ff92d42cc1fdbe2e3d1371797cf52e059ec6a
Sant99Z <santinod008@gmail.com>
2024-10-20 10:28:29 -0300

    Se agregan los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
-import { useState } from 'react'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   
 
   return (
+    <>
       <div>
+<<<<<<< HEAD
      <header class="hero-section">
         <div class="container">
             <h1>Bienvenido a Mi Página</h1>
@@ -17,27 +19,29 @@ function App() {
 
     <section class="features">
         <div class="container">
-            <h2>Nuestras Características</h2>
+            
             <div class="feature-item">
-                <h3>Calidad</h3>
-                <p>Proporcionamos contenido de la mejor calidad para ayudarte a alcanzar tus metas.</p>
+                
             </div>
             <div class="feature-item">
-                <h3>Soporte 24/7</h3>
-                <p>Contamos con un equipo de soporte listo para ayudarte en cualquier momento.</p>
+               
             </div>
             <div class="feature-item">
-                <h3>Acceso Ilimitado</h3>
-                <p>Accede a todos nuestros recursos sin limitaciones desde cualquier dispositivo.</p>
+            
             </div>
         </div>
     </section>
 
     <footer class="footer">
-        <p>&copy; 2024 Mi Página. Todos los derechos reservados.</p>
+        
     </footer>
       
+=======
+        <h1> Login </h1>
+        
+>>>>>>> parent of de3e320 (Se crea la Landing Page)
         </div>
+    </>
   )
 }
 
